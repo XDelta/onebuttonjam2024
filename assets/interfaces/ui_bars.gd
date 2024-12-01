94018a080cb0d71d0db5bbfa5d697a9f3d3ce28d
Delta <delta@deltawolf.net>
2024-12-01 01:21:57 -0800

    Passive Drain

--- assets/interfaces/ui_bars.gd
+++ assets/interfaces/ui_bars.gd
@@ -8,6 +8,12 @@ signal reset
 func _ready() -> void:
 	self.update_health.connect(_on_update_health)
 	self.reset.connect(_on_reset)
+	#timer for passive drain
+	var UI_timer = Timer.new()
+	UI_timer.wait_time = 5
+	UI_timer.autostart = true
+	add_child(UI_timer)
+	UI_timer.timeout.connect(_on_uitimer_interval)
 
 func _on_reset() -> void:
 	value = 100
@@ -21,3 +27,11 @@ func _on_update_health(_value: float) -> void:
 	var tween = create_tween()
 	tween.tween_property($HungerOver, "value", value, 0.2).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN_OUT)
 	tween.tween_property($HungerUnder, "value", value, 0.3).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
+
+func _process(delta: float) -> void:
+	pass
+
+func _on_uitimer_interval():
+	if (Globals.Started == false):
+		return
+	emit_signal("update_health", value - 10.0)
